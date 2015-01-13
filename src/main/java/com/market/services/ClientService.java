bfd0a12f0277abf520fa7c6d0b1d6adbdf2730ff
Andrey Salkov <salkov.andrey@gmail.com>
2015-01-13 19:24:10 +0300

    Doc links

--- src/main/java/com/market/services/ClientService.java
+++ src/main/java/com/market/services/ClientService.java
@@ -4,6 +4,9 @@ import com.market.model.ServiceProvider;
 
 import java.util.List;
 
+//http://spring.io/guides/gs/rest-service/
+//https://docs.jboss.org/hibernate/stable/annotations/reference/en/html_single/#d0e3524
+
 public class ClientService {
     public List<ServiceProvider> getSPOfaDay(){
         return null;
